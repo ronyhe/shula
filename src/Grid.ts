01c612e749492a110ffa2a6b559842a89b61260c
ronyhe <ronyheruth@wix.com>
2020-10-03 20:39:47 +0300

    Refactor grid to be functional

--- src/Grid.ts
+++ src/Grid.ts
@@ -3,90 +3,83 @@ interface Coordinate {
     readonly y: number
 }
 
-type Rows<T> = ReadonlyArray<ReadonlyArray<T>>
+type Grid<T> = ReadonlyArray<ReadonlyArray<T>>
 
-interface Grid<T> {
-    readonly width: number
-    readonly height: number
-    get(coordinate: Coordinate): T
-    getNeighborCoordinates(coordinate: Coordinate): ReadonlyArray<Coordinate>
-    map<A>(f: (t: T, coordinate: Coordinate) => A): Grid<A>
-    toRows(): Rows<T>
+function height<T>(grid: Grid<T>): number {
+    return grid.length
 }
 
-class GridImpl<T> implements Grid<T> {
-    readonly height: number
-    readonly width: number
+function width<T>(grid: Grid<T>): number {
+    return grid[0]?.length ?? 0
+}
 
-    constructor(private readonly items: Rows<T>) {
-        this.height = items.length
-        this.width = items[0]?.length ?? 0
-        if (!this.items.every(row => row.length === this.width)) {
-            throw new Error('All rows in a Grid must have the same width')
-        }
-    }
+function isValidCoordinate<T>(coordinate: Coordinate, grid: Grid<T>): boolean {
+    const { x, y } = coordinate
+    return x >= 0 && x < width(grid) && y >= 0 && y < height(grid)
+}
 
-    private isValidCoordinate(coordinate: Coordinate): boolean {
-        const { width, height } = this
-        const { x, y } = coordinate
-        return x >= 0 && x < width && y >= 0 && y < height
-    }
-
-    private validateCoordinate(coordinate: Coordinate): void | never {
-        if (!this.isValidCoordinate(coordinate)) {
-            const { width, height } = this
-            throw new Error(
-                `Coordinate ${coordinate} is invalid for grid with height ${height} and width ${width}`
-            )
-        }
-    }
-
-    get(coordinate: Coordinate): T {
-        this.validateCoordinate(coordinate)
-        const { x, y } = coordinate
-        return this.items[y][x]
-    }
-
-    getNeighborCoordinates(coordinate: Coordinate): ReadonlyArray<Coordinate> {
-        this.validateCoordinate(coordinate)
-        const { x, y } = coordinate
-        const above = [
-            { x: x - 1, y: y - 1 },
-            { x, y: y - 1 },
-            { x: x + 1, y: y - 1 }
-        ]
-        const sameRow = [
-            { x: x - 1, y },
-            { x: x + 1, y }
-        ]
-        const below = [
-            { x: x - 1, y: y + 1 },
-            { x, y: y + 1 },
-            { x: x + 1, y: y + 1 }
-        ]
-        const allCoordinates = [...above, ...sameRow, ...below]
-        return allCoordinates.filter(coordinate =>
-            this.isValidCoordinate(coordinate)
+function validateCoordinate<T>(
+    coordinate: Coordinate,
+    grid: Grid<T>
+): void | never {
+    if (!isValidCoordinate(coordinate, grid)) {
+        const w = width(grid)
+        const h = height(grid)
+        throw new Error(
+            `Coordinate ${coordinate} is invalid for grid with height ${h} and width ${w}`
         )
     }
-
-    map<A>(f: (t: T, coordinate: Coordinate) => A): Grid<A> {
-        return new GridImpl(
-            this.items.map((row, rowIndex) =>
-                row.map((cell, cellIndex) =>
-                    f(cell, { x: cellIndex, y: rowIndex })
-                )
-            )
-        )
-    }
-
-    toRows(): Rows<T> {
-        return this.items
-    }
 }
 
-function createGrid<T>(items: Rows<T>): Grid<T> {
-    return new GridImpl(items)
+function get<T>(coordinate: Coordinate, grid: Grid<T>): T {
+    validateCoordinate(coordinate, grid)
+    const { x, y } = coordinate
+    return grid[y][x]
 }
 
-export { Grid, Rows, Coordinate, createGrid }
+function getNeighborCoordinates<T>(
+    coordinate: Coordinate,
+    grid: Grid<T>
+): ReadonlyArray<Coordinate> {
+    validateCoordinate(coordinate, grid)
+    const { x, y } = coordinate
+    const above = [
+        { x: x - 1, y: y - 1 },
+        { x, y: y - 1 },
+        { x: x + 1, y: y - 1 }
+    ]
+    const sameRow = [
+        { x: x - 1, y },
+        { x: x + 1, y }
+    ]
+    const below = [
+        { x: x - 1, y: y + 1 },
+        { x, y: y + 1 },
+        { x: x + 1, y: y + 1 }
+    ]
+    const allCoordinates = [...above, ...sameRow, ...below]
+    return allCoordinates.filter(coordinate =>
+        isValidCoordinate(coordinate, grid)
+    )
+}
+
+function map<T, A>(
+    f: (t: T, coordinate: Coordinate) => A,
+    grid: Grid<T>
+): Grid<A> {
+    return grid.map((row, rowIndex) =>
+        row.map((cell, cellIndex) => f(cell, { x: cellIndex, y: rowIndex }))
+    )
+}
+
+export {
+    Coordinate,
+    Grid,
+    get,
+    getNeighborCoordinates,
+    map,
+    width,
+    height,
+    isValidCoordinate,
+    validateCoordinate
+}
--- test/gridTests.spec.ts
+++ test/gridTests.spec.ts
@@ -1,7 +1,15 @@
-import { Coordinate, createGrid, Grid, Rows } from '../src/Grid'
+import {
+    Coordinate,
+    Grid,
+    get,
+    getNeighborCoordinates,
+    isValidCoordinate,
+    validateCoordinate,
+    map
+} from '../src/Grid'
 
 describe('Grid', () => {
-    const rows: Rows<Coordinate> = [
+    const grid: Grid<Coordinate> = [
         [
             { x: 0, y: 0 },
             { x: 1, y: 0 },
@@ -18,43 +26,58 @@ describe('Grid', () => {
             { x: 2, y: 2 }
         ]
     ]
-    const grid: Grid<Coordinate> = createGrid(rows)
 
-    it('throws on invalid coordinates for all public methods', () => {
-        const coordinates = [
+    const validCoordinates = [
+        { x: 0, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 2 }
+    ]
+
+    describe('coordinate validation', () => {
+        const invalidCoordinates = [
             { x: -1, y: 0 },
             { x: 0, y: -1 },
             { x: 3, y: 0 },
             { x: 0, y: 3 }
         ]
-        const method = ['get', 'getNeighborCoordinates']
-        method.forEach(name => {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const method = (grid as any)[name].bind(grid)
-            coordinates.forEach(coordinate => {
-                expect(() => method(coordinate)).toThrow(
+
+        it('returns the correct validation result, and throws if needed', () => {
+            invalidCoordinates.forEach(coordinate => {
+                expect(isValidCoordinate(coordinate, grid)).toBe(false)
+                expect(() => validateCoordinate(coordinate, grid)).toThrow(
                     'is invalid for grid with height'
                 )
             })
-        })
-    })
 
-    describe('.get', () => {
-        it('returns the correct item', () => {
-            const coordinates = [
-                { x: 0, y: 0 },
-                { x: 1, y: 1 },
-                { x: 2, y: 2 }
-            ]
-            coordinates.forEach(coordinate => {
-                expect(grid.get(coordinate)).toEqual(coordinate)
+            validCoordinates.forEach(coordinate => {
+                expect(isValidCoordinate(coordinate, grid)).toBe(true)
+                expect(() => validateCoordinate(coordinate, grid)).not.toThrow()
+            })
+        })
+
+        it('throws on invalid coordinates in the relevant functions', () => {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            type GridFunc = (c: Coordinate, g: Grid<Coordinate>) => any
+            const functions: GridFunc[] = [get, getNeighborCoordinates]
+            invalidCoordinates.forEach(coordinate => {
+                functions.forEach(f => {
+                    expect(() => f(coordinate, grid)).toThrow(
+                        'is invalid for grid with height'
+                    )
+                })
             })
         })
     })
 
-    describe('.getNeighborCoordinates', () => {
+    it('returns the correct item from get', () => {
+        validCoordinates.forEach(coordinate => {
+            expect(get(coordinate, grid)).toEqual(coordinate)
+        })
+    })
+
+    describe('getNeighborCoordinates', () => {
         it('returns correct coordinates for "middle" cells', () => {
-            expect(grid.getNeighborCoordinates({ x: 1, y: 1 })).toEqual([
+            expect(getNeighborCoordinates({ x: 1, y: 1 }, grid)).toEqual([
                 { x: 0, y: 0 },
                 { x: 1, y: 0 },
                 { x: 2, y: 0 },
@@ -67,7 +90,7 @@ describe('Grid', () => {
         })
 
         it('returns correct coordinates for corner cells', () => {
-            expect(grid.getNeighborCoordinates({ x: 0, y: 0 })).toEqual([
+            expect(getNeighborCoordinates({ x: 0, y: 0 }, grid)).toEqual([
                 { x: 1, y: 0 },
                 { x: 0, y: 1 },
                 { x: 1, y: 1 }
@@ -75,13 +98,11 @@ describe('Grid', () => {
         })
     })
 
-    describe('.map', () => {
-        it('maps correctly', () => {
-            const mapped = grid.map((value, coordinate) => {
-                expect(value).toEqual(coordinate)
-                return coordinate
-            })
-            expect(mapped.toRows()).toEqual(rows)
-        })
+    it('maps correctly', () => {
+        const mapped = map((value, coordinate) => {
+            expect(value).toEqual(coordinate)
+            return coordinate
+        }, grid)
+        expect(mapped).toEqual(grid)
     })
 })
