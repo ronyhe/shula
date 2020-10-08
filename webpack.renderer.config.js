001b20b5451809bccfa1a7bcf9aec6dd28d1dcc1
ronyhe <ronyheruth@wix.com>
2020-10-08 21:52:24 +0300

    Add font

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -2,17 +2,31 @@
 const rules = require('./webpack.rules')
 const plugins = require('./webpack.plugins')
 
-rules.push({
-    test: /\.css$/,
-    use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-})
+rules.push(
+    {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+    },
+    {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'assets/fonts/'
+                }
+            }
+        ]
+    }
+)
 
 module.exports = {
     module: {
-        rules,
+        rules
     },
     plugins: plugins,
     resolve: {
-        extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
-    },
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
+    }
 }
