b3dca95e7f6537e7b9fa9221b6b8f0e951dd21b0
ronyhe <ronyheruth@wix.com>
2020-10-14 19:58:24 +0300

    Remove font that is no longer in use

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -8,18 +8,6 @@ rules.push(
         use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
     },
     {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-            {
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: 'assets/fonts/'
-                }
-            }
-        ]
-    },
-    {
         test: /\.png$/,
         use: {
             loader: 'file-loader',
