21a932f1fbc910fd88b8ccbc6a86d297c474adae
ronyhe <ronyheruth@wix.com>
2020-10-14 20:27:04 +0300

    Minor refactor

--- src/mouseButtons.ts
+++ src/mouseButtons.ts
@@ -1,11 +1,13 @@
-function isLeftClicked(buttons: number): boolean {
-    const mask = 0b01
+function isClicked(mask: number, buttons: number): boolean {
     return (mask & buttons) === mask
 }
 
+function isLeftClicked(buttons: number): boolean {
+    return isClicked(0b01, buttons)
+}
+
 function isRightClicked(buttons: number): boolean {
-    const mask = 0b10
-    return (mask & buttons) === mask
+    return isClicked(0b10, buttons)
 }
 
 export { isLeftClicked, isRightClicked }
